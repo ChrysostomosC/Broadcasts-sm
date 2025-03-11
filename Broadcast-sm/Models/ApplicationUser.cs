bdb07f07c823c369abdb6d2ef17711d9a8769f69
ChrysostomosC <153235540+ChrysostomosC@users.noreply.github.com>
2025-03-11 22:37:26 +0100

    Added default profile picture

--- Broadcast-sm/Models/ApplicationUser.cs
+++ Broadcast-sm/Models/ApplicationUser.cs
@@ -5,7 +5,7 @@ namespace Broadcast_sm.Models
     public class ApplicationUser : IdentityUser
     {
         public string? Name { get; set; }
-        public string ProfilePic { get; set; } // 2. 
+        public string ProfilePic { get; set; } = "/Images/default-profile.png"; // 2. 
         public ICollection<Broadcast> Broadcasts { get; set; }
         public ICollection<ApplicationUser> ListeningTo { get; set; } = new List<ApplicationUser>();
     }
